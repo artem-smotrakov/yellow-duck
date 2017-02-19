40047d1f9351dd83692f22f4249a1ea7a4e4828d
Artem Smotrakov <artem.smotrakov@gmail.com>
2017-02-18 21:04:24 -0800

    Save wifi ssid and password from web form

--- main.py
+++ main.py
@@ -8,7 +8,19 @@ import ussl as ssl
 CONTENT = b"""\
 HTTP/1.0 200 OK
 
-Hello #%d from MicroPython!
+<html>
+ <head>
+  <title>Yellow Duck configuration</title>
+ </head>
+ <body>
+  <form method="post">
+   Enter SSID and password:</br>
+   SSID:&nbsp;<input name="ssid" type="text"/></br>
+   Password:&nbsp;<input name="pass" type="password"/></br>
+   <input type="submit" value="Submit">
+  </form>
+ </body>
+</html>
 """
 
 # base on https://github.com/micropython/micropython/blob/master/examples/network/http_server_ssl.py
@@ -23,9 +35,8 @@ def start_local_server(use_stream=True):
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.bind(addr)
     s.listen(5)
-    print("Listening, connect your browser to https://<this_host>:8443/")
+    print("Listening, connect your browser to https://192.168.4.1:8443/")
 
-    counter = 0
     while True:
         res = s.accept()
         client_s = res[0]
@@ -43,23 +54,45 @@ def start_local_server(use_stream=True):
             # next request they issue will likely be more well-behaving and
             # will succeed.
             try:
-                req = client_s.readline()
+                req = client_s.readline().decode('utf-8')
                 print(req)
+                length = 0
                 while True:
                     h = client_s.readline()
                     if h == b"" or h == b"\r\n":
                         break
-                    print(h)
+                    header = h.decode('utf-8').lower()
+                    if header.startswith('content-length'):
+                        length = int(header.split(':')[1])
+                    print(header)
+                if req.startswith('POST') and length > 0:
+                    data = client_s.read(length).decode('utf-8')
+                    print('Data ' + data)
+                    if data:
+                        params = data.split('&')
+                        ssid = None
+                        password = None
+                        for param in params:
+                            if param.startswith('ssid='):
+                                ssid = param.split('=')[1]
+                            if param.startswith('pass='):
+                                password = param.split('=')[1]
+                        if ssid and password:
+                            write_wifi_config(ssid, password)
+                # print html form
                 if req:
-                    client_s.write(CONTENT % counter)
+                    client_s.write(CONTENT)
             except Exception as e:
                 print("Exception serving request:", e)
         else:
             print(client_s.recv(4096))
-            client_s.send(CONTENT % counter)
+            client_s.send(CONTENT)
         client_s.close()
-        counter += 1
-        print()
+
+def write_wifi_config(ssid, password):
+    f = open('wifi.conf', 'w')
+    f.write(ssid + '/' + password)
+    f.close()
 
 def start_access_point():
     import network
